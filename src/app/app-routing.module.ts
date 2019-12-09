f2aa79c0e05a11a3ca720217077d2eac5343cab3
Gordon-Allen <gordonmichaelallen@gmail.com>
2019-12-09 16:59:07 -0500

    Removed unneeded code in app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ArtistListComponent } from './artists/artist-list/artist-list.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 
 
 const routes: Routes = [
   { path: "", redirectTo: "/welcome", pathMatch: "full" },
   { path: "welcome", component: WelcomeComponent },
-  // { path: "artists", component: ArtistListComponent },
 
   {
     path: "artists", 
