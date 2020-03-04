489f6f2f788e57e15fc92ce6bb5f18daf1cbd8f0
Gordon-Allen <gordonmichaelallen@gmail.com>
2020-03-04 14:05:49 -0500

    Added functional YTP routing/logic, 'CuratedTrack'

--- src/app/artists/artist-curated-track-youtube-player/artist-curated-track-youtube-player.component.ts
+++ src/app/artists/artist-curated-track-youtube-player/artist-curated-track-youtube-player.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Artist, Album, Track } from '../artist.model';
+
+import { ActivatedRoute } from '@angular/router';
+import { ArtistService } from '../shared/artist.service';
+
 
 @Component({
   selector: 'app-artist-curated-track-youtube-player',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-curated-track-youtube-player.component.css']
 })
 export class ArtistCuratedTrackYoutubePlayerComponent implements OnInit {
+  artist: Artist;
+  album: Album;
+  track: Track;
+  id: number;
+  trackId: any;
+  trackUrl: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private artistService: ArtistService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.trackUrl = this.route.snapshot.paramMap.get("trackUrl");
+
+    const id = +this.route.snapshot.paramMap.get("id");
+
+    this.artistService.getArtistById(id).subscribe(
+      artist => {
+        this.artist = artist;
+      },
+      error => console.log("Error ", error)
+    );
+
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
   }
-
 }
--- src/app/artists/artists-routing.module.ts
+++ src/app/artists/artists-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ArtistListComponent } from './artist-list/artist-list.component';
 import { ArtistDetailsComponent } from './artist-details/artist-details.component';
+import { ArtistCuratedTrackYoutubePlayerComponent } from './artist-curated-track-youtube-player/artist-curated-track-youtube-player.component';
 import { ArtistTrackYoutubePlayerComponent } from './artist-track-youtube-player/artist-track-youtube-player.component';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: ArtistListComponent },
   { path: ":id", component: ArtistDetailsComponent },
-  { path: ":id/:trackUrl", component: ArtistTrackYoutubePlayerComponent },
+  { path: ":id/:trackUrl", component: ArtistCuratedTrackYoutubePlayerComponent },
   { path: ":id/:albumId/:trackId/:trackUrl", component: ArtistTrackYoutubePlayerComponent }
 
   ];
